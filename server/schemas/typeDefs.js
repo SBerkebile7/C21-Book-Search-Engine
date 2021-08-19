09373eac1bbe80eadf6e8048618b49906ef16da9
Spenny Berks <sberkebile@live.com>
2021-08-19 09:56:57 -0400

    fix for save book

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -13,7 +13,7 @@ const typeDefs = gql`
         image: String
         link: String
     }
-    input saveBook {
+    input bookInput {
         bookId: String
         authors: [String]
         description: String
@@ -24,7 +24,7 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(input: saveBook): User
+        saveBook(input: bookInput): User
         removeBook(bookId: String!): User
     }
     type Query {
